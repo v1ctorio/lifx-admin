ca9446cb919b00e8818f317a9bd301db6806fe11
Vic <74506415+v1ctorio@users.noreply.github.com>
2023-07-04 11:07:41 +0200

    🏷️ add lights types

---
+++ src/types.ts
@@ -0,0 +1,53 @@
+export interface light {
+    id: string
+    uuid: string
+    label: string
+    connected: boolean
+    power: string
+    color: Color
+    brightness: number
+    effect: string
+    group: Group
+    location: Location
+    product: Product
+    last_seen: string
+    seconds_since_seen: number
+  }
+  
+  export interface Color {
+    hue: number
+    saturation: number
+    kelvin: number
+  }
+  
+  export interface Group {
+    id: string
+    name: string
+  }
+  
+  export interface Location {
+    id: string
+    name: string
+  }
+  
+  export interface Product {
+    name: string
+    identifier: string
+    company: string
+    vendor_id: number
+    product_id: number
+    capabilities: Capabilities
+  }
+  
+  export interface Capabilities {
+    has_color: boolean
+    has_variable_color_temp: boolean
+    has_ir: boolean
+    has_hev: boolean
+    has_chain: boolean
+    has_matrix: boolean
+    has_multizone: boolean
+    min_kelvin: number
+    max_kelvin: number
+  }
+  
