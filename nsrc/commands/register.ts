e327528a23a7891128db0fb57c3f9d928870d12d
Vic <74506415+v1ctorio@users.noreply.github.com>
2024-06-24 23:56:28 +0000

    ¨¨¨finish¨¨ register command (pseudo??)

--- nsrc/commands/register.ts
+++ nsrc/commands/register.ts
@@ -1,45 +1,46 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, ChatInputCommandInteraction, CommandInteraction, ComponentType, DiscordAPIError, EmbedBuilder } from "discord.js";
+import { ActionRowBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, ChatInputCommandInteraction, CommandInteraction, ComponentType, DiscordAPIError, EmbedBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } from "discord.js";
+import { updateLanguageServiceSourceFile } from "typescript";
 
 async function register(commandI: ChatInputCommandInteraction) {
     const loginEmbed = new EmbedBuilder()
-        .setTitle( "Log in with LIFX")
+        .setTitle("Log in with LIFX")
         .setDescription(
             `In order to login into lifix, you'll need to give your lifx cloud token.`
-          )
-          .setColor("#450098")
-          .addFields([
+        )
+        .setColor("#450098")
+        .addFields([
             {
-              name: "How to get your token?",
-              value:
-                'Go to [lifx cloud](https://cloud.lifx.com/settings), login and click on "Generate new token".',
+                name: "How to get your token?",
+                value:
+                    'Go to [lifx cloud](https://cloud.lifx.com/settings), login and click on "Generate new token".',
             },
             {
-              name: "How to use the token?",
-              value:
-                'Click on the button "login" below and paste your token into the modal.',
+                name: "How to use the token?",
+                value:
+                    'Click on the button "login" below and paste your token into the modal.',
             },
-          ]);
+        ]);
 
     const loginEmbedActionsRow = new ActionRowBuilder<ButtonBuilder>()
-    .addComponents(
-      new ButtonBuilder()
-        .setCustomId("login")
-        .setLabel("Login")
-        .setStyle(ButtonStyle.Primary)
-    )
-    .addComponents(
-      new ButtonBuilder()
-        .setURL("https://cloud.lifx.com/settings")
-        .setLabel("Lifx cloud settings")
-        .setStyle(ButtonStyle.Link)
-    );
+        .addComponents(
+            new ButtonBuilder()
+                .setCustomId("login")
+                .setLabel("Login")
+                .setStyle(ButtonStyle.Primary)
+        )
+        .addComponents(
+            new ButtonBuilder()
+                .setURL("https://cloud.lifx.com/settings")
+                .setLabel("Lifx cloud settings")
+                .setStyle(ButtonStyle.Link)
+        );
 
 
     const timeoutEmbed = new EmbedBuilder()
-    .setTitle("Timeout")
-    .setDescription("You took too long to click the button. Please run the command again.")
-    .setColor("#FF0000");
-    
+        .setTitle("Timeout")
+        .setDescription("You took too long to click the button. Please run the command again.")
+        .setColor("#FF0000");
+
     const response = await commandI.reply({
         embeds: [loginEmbed],
         components: [loginEmbedActionsRow]
@@ -47,7 +48,7 @@ async function register(commandI: ChatInputCommandInteraction) {
     const Bcollector = response.createMessageComponentCollector({
         componentType: ComponentType.Button,
         time: 60000,
-        filter: (interaction) => interaction.customId === "login"  && interaction.user.id === commandI.user.id,
+        filter: (interaction) => interaction.customId === "login" && interaction.user.id === commandI.user.id,
         onError: (error) => {
             if (error instanceof DiscordAPIError) {
                 console.error(error);
@@ -63,13 +64,55 @@ async function register(commandI: ChatInputCommandInteraction) {
             await interaction.deferUpdate();
             await interaction.followUp("Please paste your token here.");
         }
-    })
-    Bcollector.on("end",collected=> {
-        if (collected.size === 0) return commandI.editReply({
-                                    embeds: [timeoutEmbed],
-                                    components : []
-                                    })
     });
+    Bcollector.on("end", collected => {
+        if (collected.size === 0) {
+            commandI.editReply({
+                embeds: [timeoutEmbed],
+                components: []
+            });
+        }
+    });
+    Bcollector.on("collect", async (interaction: ButtonInteraction) => {
+        const loginmodal = new ModalBuilder()
+        .setTitle("Login into lifx")
+        .setCustomId("lifxloginmodal");
+
+      const TokenInput = new TextInputBuilder()
+        .setLabel("Token")
+        .setCustomId("tokenInput")
+        .setPlaceholder("Paste your token here")
+        .setStyle(TextInputStyle.Short);
+
+      const actionRow =
+        new ActionRowBuilder<TextInputBuilder>().addComponents(
+          TokenInput
+        );
+
+      loginmodal.addComponents(actionRow);
+      interaction.showModal(loginmodal);
+      
+      interaction.awaitModalSubmit({time:60000})
+      .then(async (modal) => {
+        const token = modal.fields.getTextInputValue("tokenInput");
+        updateToken(token, interaction.user.id); //TODO with db
+        interaction.editReply({
+            content: "Registered into LIFX bot successfully",
+            components: []});
+        })
+        .catch((error) => {
+            console.error(error);
+            interaction.editReply({
+                content: "You took too long to paste the token. Please try again.",
+                components: []
+            });
+        }
+        )
+
+
+    })
     
+
+
 
 }
