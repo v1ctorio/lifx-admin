e094c6ab6738e3d1b6d4fab259b29050f600cdea
vic <74506415+v1ctorio@users.noreply.github.com>
2024-08-22 11:06:57 +0200

    use REDIS_PORT and LIFXA_REDIS_DB

--- src/db/loaddb.ts
+++ src/db/loaddb.ts
@@ -1,7 +1,10 @@
 import { createClient } from "redis";
 
 async function loadDB() {
-const redisClient = createClient();
+const redisClient = createClient({
+  database: process.env.LIFXA_REDIS_DB ? parseInt(process.env.LIFXA_REDIS_DB) : 0,
+  url: `redis://localhost:${process.env.REDIS_PORT}`,
+});
 redisClient.on("error", (err) => {
   console.error(err);
 });
